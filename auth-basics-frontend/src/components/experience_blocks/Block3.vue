a804d277bc65feb0c70119d645145de45bf47554
Gepiroy <koderkokoderov@mail.ru>
2024-03-07 17:41:38 +0300

    Scroll map and some fixes

--- auth-basics-frontend/src/components/experience_blocks/Block3.vue
+++ auth-basics-frontend/src/components/experience_blocks/Block3.vue
@@ -1,5 +1,5 @@
 <script setup>
-import StandPoint from './StandPoint.vue';
+import StandPoint from "./StandPoint.vue";
 </script>
 
 <template>
@@ -8,69 +8,95 @@ import StandPoint from './StandPoint.vue';
     lore="Тут я хочу сделать интерактивную карту мероприятия. На ней будут точки, нажатие по которым откроет окна 
     с названием мероприятий. Сами точки и мероприятия будут доставаться из локальной базы-массива."
   >
-    <div class="map-frame">
-      <div class="map-holder" :style="'transform: translate('+(x)+'px, '+(y)+'px)'" @mousemove="mouseMove" @mousedown="mouseDown" @mouseup="mouseUp">
-        <img src="@/assets/map.jpg" :style="''+mapStyle()" draggable="false"/>
-        <stand-point v-for="(point, index) in points" :key="index" :style="'top:'+((point.y-5)*scale)+'px; left: '+((point.x-5)*scale)+'px;'" :title="point.title" :lore="point.lore"/>
+    <div class="map-frame"
+        @mousemove="mouseMove"
+        @mousedown="mouseDown"
+        @mouseup="mouseUp"
+        @wheel.prevent="wheelMove">
+      <div
+        class="map-holder"
+        :style="'transform: translate(' + x + 'px, ' + y + 'px)'"
+      >
+        <img
+          src="@/assets/map.jpg"
+          :style="'' + mapStyle()"
+          draggable="false"
+        />
+        <stand-point
+          v-for="(point, index) in points"
+          :key="index"
+          :style="
+            'top:' +
+            (point.y - 5) * scale +
+            'px; left: ' +
+            (point.x - 5) * scale +
+            'px;'
+          "
+          :title="point.title"
+          :lore="point.lore"
+        />
       </div>
     </div>
   </block-base>
 </template>
 
 <style scoped>
-  .map-frame{
-    position: relative;
-    border: 1px white solid;
-    padding: 2px;
-    max-width: 600px;
-    max-height: 600px;
-    overflow: hidden;
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
-  }
-  .map{
-    
-  }
+.map-frame {
+  position: relative;
+  border: 1px white solid;
+  padding: 2px;
+  max-width: 600px;
+  max-height: 600px;
+  overflow: hidden;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+.map {
+}
 </style>
 
 <script>
 export default {
   methods: {
-    mapStyle(){
-      let scale = this.scale
-      let ret = 'transform: '
-      let translate = (scale-1.0)*50
-      ret += 'translate('+translate+'%, '+translate+'%) '
-      ret += 'scale('+scale+');'
-      console.log("map style is "+ret)
+    mapStyle() {
+      let scale = this.scale;
+      let ret = "transform: ";
+      let translate = (scale - 1.0) * 50;
+      ret += "translate(" + translate + "%, " + translate + "%) ";
+      ret += "scale(" + scale + ");";
+      console.log("map style is " + ret);
       return ret;
     },
-    mapHolderStyle(){
-      let scale = this.scale
-      let x = this.x
-      let y = this.y
-      let ret = 'transform: translate()'
-      let translate = (scale-1.0)*50
-      ret += 'translate('+translate+'%, '+translate+'%) '
-      ret += 'scale('+scale+');'
-      console.log("map style is "+ret)
+    mapHolderStyle() {
+      let scale = this.scale;
+      let x = this.x;
+      let y = this.y;
+      let ret = "transform: translate()";
+      let translate = (scale - 1.0) * 50;
+      ret += "translate(" + translate + "%, " + translate + "%) ";
+      ret += "scale(" + scale + ");";
+      console.log("map style is " + ret);
       return ret;
     },
     mouseDown(e) {
-      this.dragging = true
+      this.dragging = true;
     },
     mouseUp(e) {
-      this.dragging = false
+      this.dragging = false;
     },
     mouseMove(e) {
-      if(this.dragging){
-        this.x += e.movementX
-        this.y += e.movementY
-        if(this.x>0)this.x=0
-        if(this.y>0)this.y=0
+      if (this.dragging) {
+        this.x += e.movementX;
+        this.y += e.movementY;
+        if (this.x > 0) this.x = 0;
+        if (this.y > 0) this.y = 0;
       }
-    }
+    },
+    wheelMove(e) {
+      this.scale -= e.deltaY * 0.001; //10%
+      if (this.scale < 0.2) this.scale = 0.2;
+    },
   },
   data() {
     return {
@@ -96,8 +122,8 @@ export default {
       ],
       scale: 0.9,
       dragging: false,
-      x: 100,
-      y: 100
+      x: 0,
+      y: 0,
     };
   },
 };
